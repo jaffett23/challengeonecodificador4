7e09f71fbc9fed73771431b0ff7040b81f18d118
Jaffett23 <brauliorobles23@gmail.com>
2023-01-03 17:44:44 -0600

    Se hagrega la funcion de copiar y se agregan unas cosas

--- js/funciones.js
+++ js/funciones.js
@@ -1,36 +1,54 @@
-function encriptar(){
-    var texto = document.querySelector("#input_encriptar").value;
-    var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-    aparecerElementos();
-    document.getElementById("label_resultado").textContent = textoCifrado;
-    esconderElementos();
+function encriptar() {
+    if (texto = document.querySelector("#input_encriptar").value == "")
+        false;
+    else {
+        var texto = document.querySelector("#input_encriptar").value;
+        var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        aparecerElementos();
+        resultado.textContent = textoCifrado;
+        esconderElementos();
+    }
 }
 
-function aparecerElementos(){
+function aparecerElementos() {
     document.getElementById("label_resultado").style.display = "flex";
     document.getElementById("button_copiar").style.display = "flex";
 }
 
-function esconderElementos(){
+function esconderElementos() {
 
     document.getElementById("frame5").style.display = "none";
     document.getElementById("muñeco").style.display = "none";
 }
 
-function limpiar(){
-    document.getElementById("label_resultado").style.display = "none";
-    document.getElementById("button_copiar").style.display = "none";
-    document.getElementById("frame5").style.display = "flex";
-    document.getElementById("muñeco").style.display = "flex";
-    document.querySelector("#input_encriptar").value = " ";
-    ponerCursor();
+function limpiar() {
+    if (texto = document.querySelector("#input_encriptar").value == "")
+        false;
+    else {
+        document.getElementById("label_resultado").style.display = "none";
+        document.getElementById("button_copiar").style.display = "none";
+        document.getElementById("frame5").style.display = "flex";
+        document.getElementById("muñeco").style.display = "flex";
+        document.querySelector("#input_encriptar").value = "";
+        ponerCursor();
+    }
+}
+
+function copiar() {
+    let txtCopiar = document.querySelector('#label_resultado').innerHTML;
+    navigator.clipboard.writeText(txtCopiar);
 }
 
 function ponerCursor() {
     document.getElementById('input_encriptar').focus();
-  }
+}
+
+var resultado = document.getElementById("label_resultado");
+
 window.onload = ponerCursor;
 var boton_encriptar = document.querySelector("#button_encriptar");
 boton_encriptar.onclick = encriptar;
-var boton_encriptar = document.querySelector("#button_limpiar");
-boton_encriptar.onclick = limpiar;
+var boton_limpiar = document.querySelector("#button_limpiar");
+boton_limpiar.onclick = limpiar;
+var boton_copiar = document.querySelector("#button_copiar");
+boton_copiar.onclick = copiar;
